542498735027ae040012283eb75d5714e2339e34
korayekici <55810414+korayekici@users.noreply.github.com>
2019-10-16 00:12:11 +0300

    Add files via upload

---
+++ hw3.py
@@ -0,0 +1,9 @@
+old = float(input("how old are you:"))
+if old > 19:
+    print("adult")
+if old <= 19:
+    print ("children")
+if 10 < old <= 19:
+    print("adolescent")
+if old < 1:
+    print("infant")
